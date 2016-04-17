958d54ec2209a2551d5cedec2bc5035c19426094
Emil Vanherp <emil.vanherp@hotmail.com>
2016-04-17 23:34:26 +0800

    WIP UI, keyboard input

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 import sys
 import vlc
-from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication)
+from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication, QGridLayout,
+        QLabel, QLineEdit)
 
 from AudioFile import AudioFile
 
@@ -17,21 +18,33 @@ class Window(QWidget):
 
     def initUI(self):
         grid = QGridLayout()
+        play = QPushButton('Play', self)
+        check = QPushButton('Check', self)
+        self.answerEdit = QLineEdit()
+        title = QLabel('answer')
+
         self.setLayout(grid)
 
-        play = QPushButton('Play', self)
         play.clicked.connect(self.play)
-
-        check = QPushButton('Check', self)
         check.clicked.connect(self.check)
+        self.answerEdit.returnPressed.connect(self.check)
 
+        grid.addWidget(title,0,1)
+        grid.addWidget(self.answerEdit,0,2)
+        grid.addWidget(play,1,1)
+        grid.addWidget(check,1,2)
+
+        self.answerEdit.setFocus()
+
+        print(self.__dict__)
         self.show()
 
     def play(self):
         self.audiofile.play()
 
     def check(self):
-        self.audiofile.check_answer()
+        self.audiofile.check_answer(self.answerEdit.text())
+        self.answerEdit.clear()
 
 
 if __name__ == '__main__':
