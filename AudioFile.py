b4ae4ba1b4ec098571c8260fb63bc7212e3c363c
Emil Vanherp <emil.vanherp@hotmail.com>
2016-05-08 00:32:20 +0800

    style cleanup

--- AudioFile.py
+++ AudioFile.py
@@ -1,26 +1,42 @@
-from vlc import MediaPlayer
+"""
+AudioFile class and all it's helper functions
+"""
 from os import walk
 from random import choice
-
-audio_dir = "audio"
-syllable_seperator = "_"
+from vlc import MediaPlayer
+AUDIO_DIR = "audio"
 
 class AudioFile():
+    """
+    Class for the audiofile.
+    Provides all methods to interact with the audio:
+    play, check answer, ...
+    """
     def __init__(self):
-        self.__filename = self.read_file()
-        self.__audio = MediaPlayer(audio_dir + "/" + self.__filename)
+        self.__filename = _read_file()
+        self.__audio = MediaPlayer(AUDIO_DIR + "/" + self.__filename)
         self.play()
 
     def play(self):
+        """play the audio of the file"""
         self.__audio.stop()
         self.__audio.play()
 
     def check_answer(self, answer):
-        return answer == self.__filename.split("__")[0].replace("_","")
-
-    def read_file(self):
-        _, _, filenames = next(walk(audio_dir))
-        return choice(filenames)
+        """
+        Check if the provided answer is correct.
+        The format should be sound + tone and no seperator between syllables
+        ie shang4hai3
+        """
+        return answer == self.__filename.split("__")[0].replace("_", "")
 
     def __del__(self):
         self.__audio.release()
+
+def _read_file():
+    """
+    return a random filename from the audio directory
+    """
+    _, _, filenames = next(walk(AUDIO_DIR))
+    return choice(filenames)
+
