abd7c4d732670e9b7b25dba6fa9b21d50f5fecd9
Emil Vanherp <emil.vanherp@protonmail.ch>
2016-11-13 21:26:02 +0800

    feat: Add debug module for easy pdb debugging
    Start pdb with import debug; debug.debug_trace()

---
+++ debug.py
@@ -0,0 +1,8 @@
+from PyQt5.QtCore import pyqtRemoveInputHook
+from pdb import set_trace
+
+def debug_trace():
+  '''Set a tracepoint in the Python debugger that works with Qt'''
+
+  pyqtRemoveInputHook()
+  set_trace()
